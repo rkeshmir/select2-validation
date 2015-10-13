dd5d4182349ca5d27bbb4c749db0f8435735b323
Reza Keshmir <rezacashmere@gmail.com>
2015-10-13 18:52:37 +0330

    initiate node and bower and gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/**
+ * Created by reza on 10/13/15.
+ */
+var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    mainBowerFiles = require('gulp-main-bower-files'),
+    gulpFilter = require('gulp-filter'),
+    rename = require("gulp-rename");
+
+gulp.task('bower', function () {
+    return gulp.src('./bower.json') //  read all bower files
+        .pipe(mainBowerFiles({
+            overrides: {
+                bootstrap: {
+                    main: [
+                        './dist/**/*.min.*',
+                        './dist/fonts/*'
+                    ]
+                }  //  consider these files for bootstrap
+            }
+        })) //  select main files
+        .pipe(gulp.dest('./dist/bower_components')); //  copy concatenated file to destination
+});
+
+gulp.task('clean', function () {
+    return gulp.src('./dist', {read: false})
+        .pipe(clean());
+});
+
