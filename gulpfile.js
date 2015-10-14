9f14ff519e23b7d097bd0df9dd7efcd8658b98c1
Reza Keshmir <rezacashmere@gmail.com>
2015-10-14 14:04:56 +0330

    First stable commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,17 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
     mainBowerFiles = require('gulp-main-bower-files'),
-    gulpFilter = require('gulp-filter'),
+    series = require('stream-series'),
+    inject = require('gulp-inject'),
     rename = require("gulp-rename");
 
-gulp.task('bower', function () {
+
+gulp.task('clean', function () {
+    return gulp.src('./dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('bower', ['clean'], function () {
     return gulp.src('./bower.json') //  read all bower files
         .pipe(mainBowerFiles({
             overrides: {
@@ -22,12 +29,19 @@ gulp.task('bower', function () {
         .pipe(gulp.dest('./dist/bower_components')); //  copy concatenated file to destination
 });
 
-gulp.task('clean', function () {
-    return gulp.src('./dist', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('dist', function () {
+gulp.task('html', ['bower'], function () {
     return gulp.src('*.html')
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['html'], function () {
+    var target = gulp.src('./dist/index.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var jQueryStream = gulp.src(['dist/bower_components/jquery/dist/jquery.js'], {read: false}, {relative: true});
+
+    var vendorStream = gulp.src(['dist/**/*.js','dist/**/*.css','!dist/bower_components/jquery/dist/jquery.js'], {read: false}, {relative: true});
+
+
+    return target.pipe(inject(series(jQueryStream, vendorStream) , {relative: true}))
+        .pipe(gulp.dest('./dist'));
 });
